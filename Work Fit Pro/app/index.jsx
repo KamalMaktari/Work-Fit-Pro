62675d3f507f19460581718911925465a39c0394
Kamal Maktari <kamalmaktari@gmail.com>
2023-12-25 01:59:01 +0000

    Installed New Libraries
    Installed 'react-native-responsive-screen' and 'expo-linear-gradient' to support in styling and making the app responsive.

--- Work Fit Pro/app/index.jsx
+++ Work Fit Pro/app/index.jsx
@@ -1,5 +1,6 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function Index() {
   return (
