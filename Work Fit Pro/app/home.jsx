f0db30ca1098258d8bd0fd081c58e2aac241bdfe
Kamal Maktari <kamalmaktari@gmail.com>
2023-12-26 14:04:48 +0000

    Started Designing Home Screen
    Added to home screen - title, profile picture, and notification bell. Also installed libraries to help with the design of the page.

---
+++ Work Fit Pro/app/home.jsx
@@ -0,0 +1,51 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+export default function Home() {
+	return (
+		<SafeAreaView className="flex-1 bg-white flex space-y-5" edges={['top']}>
+			<StatusBar style='dark' />
+			
+			{/* punchline and avatar */}
+			<View className='flex-row justify-between item-center mx-5'>
+				<View className='spacy-y-2'>
+					<Text
+						style={{fontSize: hp(4.5)}}
+						className='font-bold tracking-wider text-neutral-700'
+					>
+						READY TO
+					</Text>
+					<Text
+						style={{fontSize: hp(4.5)}}
+						className='font-bold tracking-wider text-rose-500'
+					>
+						WORKOUT
+					</Text>
+				</View>
+
+				<View className='flex justify-center items-center space-y-2'>
+					<Image 
+						source={require('../assets/images/avatar.png')}
+						style={{height: hp(6), width: hp(6)}} 
+						className='rounded-full'
+					/>
+					<View 
+						className='bg-neutral-200 rounded-full flex justify-center items-center border-[3px] border-neutral-300'
+						style={{height: hp(5.5), width: hp(5.5)}}
+					>
+						<Ionicons name="notifications" size={hp(3)} color="gray" />
+					</View>
+				</View>
+			</View>
+			
+			{/* image slider */}
+			<View>
+				
+			</View>
+		</SafeAreaView>
+	)
+}
--- Work Fit Pro/app/index.jsx
+++ Work Fit Pro/app/index.jsx
@@ -4,8 +4,10 @@ import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-nativ
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
 import Animated, { FadeIn, FadeInDown, FadeOut } from 'react-native-reanimated';
+import { useRouter } from 'expo-router';
 
 export default function Index() {
+	const router = useRouter();
   return (
     <View className="flex-1 flex justify-end">
       	<StatusBar style='light' />
@@ -29,6 +31,7 @@ export default function Index() {
 
 			<Animated.View entering={FadeInDown.delay(200).springify()}>
 				<TouchableOpacity
+					onPress={()=> router.push('home')}
 					style={{height: hp(7), width: wp(80)}}
 					className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
 				>
