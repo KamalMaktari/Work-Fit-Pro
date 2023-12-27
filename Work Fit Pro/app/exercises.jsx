1e71a5783fd60e93fe6b24057c32aa3f8a9552e4
Kamal Maktari <kamalmaktari@gmail.com>
2023-12-27 18:27:35 +0000

    Fetched API Data to Gather Exercise Info
    Rapid API was used to fetch data.

--- Work Fit Pro/app/exercises.jsx
+++ Work Fit Pro/app/exercises.jsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { fetchExercisesByBodypart } from '../api/exerciseDB';
+import { demoExercises } from '../constants';
+import { StatusBar } from 'expo-status-bar';
 
 export default function Exercises() {
+	const router = useRouter ();
+	const [exercises, setExercises] = useState (demoExercises);
+	const item = useLocalSearchParams();
+	console.log('Received Item: ', item);
+
+	useEffect(() => {
+		// if (item) getExercises(item.name);
+	}, [item]);
+
+	const getExercises = async (bodypart) => {
+		let data = await fetchExercisesByBodypart(bodypart);
+		// console.log('Received Data:', data);
+		setExercises(data);
+	}
   return (
-    <View>
-      <Text>Exercises</Text>
-    </View>
+	<ScrollView>
+		<StatusBar style='light' />
+		
+	</ScrollView>
   )
 }
